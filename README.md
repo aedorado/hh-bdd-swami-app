e05632456def3ea886c44ce9911f035109146382
aedorado <annurag94@gmail.com>
2021-05-13 14:56:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hh_bbds_app
+
+The official app for His Holiness Bhakti Dhira Damodara Swami.
---
+++ android/app/src/main/kotlin/com/example/hh_bbds_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hh_bbds_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
