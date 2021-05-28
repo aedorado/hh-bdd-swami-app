a4d746276495444d444d34c59ef70c3ca7af3b6e
aedorado <annurag94@gmail.com>
2021-05-28 18:22:41 +0530

    Add animated pages on AudioList Screen

--- lib/ui/audio/audio_list_screen.dart
+++ lib/ui/audio/audio_list_screen.dart
@@ -13,6 +13,10 @@ class AudioListScreen extends StatefulWidget {
 
 class _AudioListScreenState extends State<AudioListScreen> {
 
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+  final _animationDuration = 250;
   int selectedSuggestion = 0;
 
   String audioAPIUrl = "https://mocki.io/v1/6817415e-fc15-4ed5-b6a2-e811e45802f5";
@@ -42,6 +46,8 @@ class _AudioListScreenState extends State<AudioListScreen> {
     super.dispose();
   }
 
+  List audioListScreenSuggestions = ['TRACKS', 'ALBUMS', 'YEAR', 'POPULARITY', 'LENGTH'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,134 +58,80 @@ class _AudioListScreenState extends State<AudioListScreen> {
         children: [
           ColoredBox(
               color: Color(0xFFBDBDBD),
-              child: Row(
-                children: [
-                  AudioListSuggestionBox(
-                    title: 'TRACKS',
-                    flex: 2,
-                    isSelected: selectedSuggestion == 0,
+              child: Container(
+                height: 56,
+                child: SingleChildScrollView(
+                  physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: audioListScreenSuggestions.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return _audioListSuggestionBox(index, audioListScreenSuggestions[index]);
+                          }
+                      ),
+                    ],
                   ),
-                  AudioListSuggestionBox(
-                    title: 'ALBUMS',
-                    flex: 2,
-                    isSelected: selectedSuggestion == 1,
-                  ),
-                  AudioListSuggestionBox(
-                    title: 'YEAR',
-                    flex: 2,
-                    isSelected: selectedSuggestion == 2,
-                  ),
-                  AudioListSuggestionBox(
-                    title: 'POPULARITY',
-                    flex: 3,
-                    isSelected: selectedSuggestion == 3,
-                  ),
-                  AudioListSuggestionBox(
-                    title: 'LENGTH',
-                    flex: 2,
-                    isSelected: selectedSuggestion == 4,
-                  ),
-                ],
-              )),
-          FutureBuilder<List<Audio>>(
-            future: fetchAudios(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                // return Text(snapshot.data!.title);
-                return Container(
-                    child: ListView.builder(
-                        scrollDirection: Axis.vertical,
-                        shrinkWrap: true,
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            height: 80,
-                            child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Expanded(
-                                    flex: 1,
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(
-                                          top: 2, left: 2, right: 2, bottom: 2),
-                                      child: CircleAvatar(
-                                          backgroundImage: NetworkImage(
-                                              'https://i.postimg.cc/RZJ6HJrw/c.jpg')),
-                                      // ClipRRect(
-                                      //   borderRadius: BorderRadius.circular(8.0),
-                                      //   child: Image.network('https://i.postimg.cc/RZJ6HJrw/c.jpg', height: 150.0, width: 100.0),
-                                      // ),
-                                    ),
-                                  ),
-                                  Expanded(
-                                      flex: 4,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(12.0),
-                                        child: Consumer<CurrentAudio>(
-                                          builder: (_, currentAudio, child) => InkWell(
-                                            onTap: () {
-                                              currentAudio.audio = snapshot.data[index];
-                                              currentAudio.currentAudioIndex = index;
-                                              Navigator.push(context, MaterialPageRoute(builder: (context) => AudioPlayScreen()));
-                                            },
-                                            child: Consumer<CurrentAudio>(
-                                              builder: (_, currentAudio, child) => Column(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment.center,
-                                                crossAxisAlignment:
-                                                    CrossAxisAlignment.start,
-                                                children: [
-                                                  Text('${snapshot.data[index].name}', style: TextStyle(fontSize: 16),),
-                                                  if (currentAudio.isPlaying && index == currentAudio.currentAudioIndex) Text(currentAudio.currentAudioPosition.toString().split('.').first),
-                                                  if (currentAudio.isPlaying && index == currentAudio.currentAudioIndex) Text(currentAudio.totalAudioDuration.toString().split('.').first),
-                                                ],
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      )),
-                                  Expanded(
-                                      flex: 1,
-                                      child: Consumer<CurrentAudio>(
-                                        builder: (_, currentAudio, child) => InkWell(
-                                              onTap: () {
-                                                // if audio is playing
-                                                // and
-                                                // user clicks on the button for the audio that is playing
-                                                // then pause audio
-                                                if (currentAudio.isPlaying && (index == currentAudio.currentAudioIndex)) {
-                                                  currentAudio.pauseAudio();
-                                                } else if (currentAudio.isPlaying && (index != currentAudio.currentAudioIndex)) { // user clicks on play button for an audio that is not playing currently
-                                                  currentAudio.stopAudio();
-                                                  currentAudio.currentAudioIndex = index;
-                                                  currentAudio.audio = snapshot.data[index];
-                                                  currentAudio.playAudio();
-                                                } else if (!currentAudio.isPlaying) { // if not audio is playing, simply start playing current audio
-                                                  currentAudio.currentAudioIndex = index;
-                                                  currentAudio.audio = snapshot.data[index];
-                                                  currentAudio.playAudio();
-                                                }
-                                              },
-                                              child: Icon((index == currentAudio.currentAudioIndex && currentAudio.isPlaying) ? Icons.pause : Icons.play_arrow)),
-                                      ),
-                                      ),
-                                  // Divider(color: Colors.black, height: 1,),
-                                ],
-                                //: Center(child: Text('${snapshot.data[index].name}', style: TextStyle(fontSize: 18),)),
-                              ),
-                          );
-                        }));
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-              return CircularProgressIndicator();
-            },
+                ),
+              ),
+
           ),
+          Expanded(
+            child: PageView.builder(
+              itemCount: audioListScreenSuggestions.length,
+              itemBuilder: (context, index) {
+                return AudioListScreenPage();
+              },
+              controller: _pageController,
+              onPageChanged: (pageNumber) {
+                setState(() {
+                  this.selectedSuggestion = pageNumber;
+                });
+              },
+            ),
+          )
+          // AudioListScreenPage(),
         ],
       ),
     );
   }
+
+  Widget _audioListSuggestionBox(int index, String title) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          this.selectedSuggestion = index;
+          this._pageController.animateToPage(index, duration: Duration(milliseconds: this._animationDuration), curve: Curves.easeIn);
+        });
+      },
+      child: Padding(
+        padding:
+        const EdgeInsets.only(left: 5.0, right: 5.0, top: 6, bottom: 6),
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: this._animationDuration),
+          curve: Curves.easeIn,
+          decoration: new BoxDecoration(
+            color: this.selectedSuggestion == index ? Color(0xFF0077C2) : Color(0xFFBDBDBD),
+            borderRadius: new BorderRadius.all(Radius.elliptical(80, 100)),
+          ),
+          height: 36,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
 }
 
 class AudioListSuggestionBox extends StatelessWidget {
@@ -193,24 +145,121 @@ class AudioListSuggestionBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-        flex: this.flex,
+    return Padding(
+      padding:
+          const EdgeInsets.only(left: 5.0, right: 5.0, top: 6, bottom: 6),
+      child: Container(
+        decoration: new BoxDecoration(
+          color: isSelected == true ? Color(0xFF0077C2) : Color(0xFFBDBDBD),
+          borderRadius: new BorderRadius.all(Radius.elliptical(80, 100)),
+        ),
+        height: 36,
         child: Padding(
-          padding:
-              const EdgeInsets.only(left: 2.0, right: 2.0, top: 6, bottom: 6),
-          child: Container(
-            decoration: new BoxDecoration(
-              color: isSelected == true ? Color(0xFF0077C2) : Color(0xFFBDBDBD),
-              borderRadius: new BorderRadius.all(Radius.elliptical(110, 100)),
-            ),
-            height: 36,
-            child: Center(
-              child: Text(
-                this.title,
-                style: TextStyle(color: Colors.white, fontSize: 15),
-              ),
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Text(
+              this.title,
+              style: TextStyle(color: Colors.white, fontSize: 16),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
+
+class AudioListScreenPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Audio>>(
+      future: fetchAudios(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          // return Text(snapshot.data!.title);
+          return Container(
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: 80,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 2, left: 2, right: 2, bottom: 2),
+                              child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      'https://i.postimg.cc/RZJ6HJrw/c.jpg')),
+                            ),
+                          ),
+                          Expanded(
+                              flex: 4,
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Consumer<CurrentAudio>(
+                                  builder: (_, currentAudio, child) => InkWell(
+                                    onTap: () {
+                                      currentAudio.audio = snapshot.data[index];
+                                      currentAudio.currentAudioIndex = index;
+                                      Navigator.push(context, MaterialPageRoute(builder: (context) => AudioPlayScreen()));
+                                    },
+                                    child: Consumer<CurrentAudio>(
+                                      builder: (_, currentAudio, child) => Column(
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                        children: [
+                                          Text('${snapshot.data[index].name}', style: TextStyle(fontSize: 16),),
+                                          if (currentAudio.isPlaying && index == currentAudio.currentAudioIndex) Text(currentAudio.currentAudioPosition.toString().split('.').first),
+                                          if (currentAudio.isPlaying && index == currentAudio.currentAudioIndex) Text(currentAudio.totalAudioDuration.toString().split('.').first),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )),
+                          Expanded(
+                            flex: 1,
+                            child: Consumer<CurrentAudio>(
+                              builder: (_, currentAudio, child) => InkWell(
+                                  onTap: () {
+                                    // if audio is playing and user clicks on the button for the audio that is playing then pause audio
+                                    if (currentAudio.isPlaying && (index == currentAudio.currentAudioIndex)) {
+                                      currentAudio.pauseAudio();
+                                    } else if (currentAudio.isPlaying && (index != currentAudio.currentAudioIndex)) { // user clicks on play button for an audio that is not playing currently
+                                      currentAudio.stopAudio();
+                                      currentAudio.currentAudioIndex = index;
+                                      currentAudio.audio = snapshot.data[index];
+                                      currentAudio.playAudio();
+                                    } else if (!currentAudio.isPlaying) { // if not audio is playing, simply start playing current audio
+                                      currentAudio.currentAudioIndex = index;
+                                      currentAudio.audio = snapshot.data[index];
+                                      currentAudio.playAudio();
+                                    }
+                                  },
+                                  child: Icon((index == currentAudio.currentAudioIndex && currentAudio.isPlaying) ? Icons.pause : Icons.play_arrow)),
+                            ),
+                          ),
+                          // Divider(color: Colors.black, height: 1,),
+                        ],
+                        //: Center(child: Text('${snapshot.data[index].name}', style: TextStyle(fontSize: 18),)),
+                      ),
+                    );
+                  }));
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        return Center(child: Container(height: 24, width: 24, child: CircularProgressIndicator()));
+      },
+    );
+  }
+}
+
+
--- lib/ui/audio/audio_play_screen.dart
+++ lib/ui/audio/audio_play_screen.dart
@@ -35,105 +35,129 @@ class _AudioPlayScreenState extends State<AudioPlayScreen> {
         ),
         child: Column(
           children: [
-            Spacer(),
-            new Padding(
-              padding:
-                  const EdgeInsets.only(left: 48, right: 48, top: 24, bottom: 12),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 48, right: 48, top: 24, bottom: 12),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.35,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://i1.sndcdn.com/artworks-000674039176-uw34hj-t500x500.jpg"),
+                          fit: BoxFit.cover)),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 50, top: 16),
+                  child: Text(
+                    "What Prahalad Maharaja learnt in the womb",
+                    style: audioTitleStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
               child: Container(
-                height: MediaQuery.of(context).size.height * 0.35,
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                        image: NetworkImage(
-                            "https://i1.sndcdn.com/artworks-000674039176-uw34hj-t500x500.jpg"),
-                        fit: BoxFit.cover)),
-              ),
-            ),
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(left: 50, right: 50, top: 16),
-              child: Text(
-                "What Prahalad Maharaja learnt in the womb",
-                style: audioTitleStyle,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Spacer(),
-            Container(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50, right: 50, top: 16),
-                    child: SliderTheme(
-                      data: SliderThemeData(
-                        // thumbColor: Color(0xFFEB1555),
-                        inactiveTrackColor: Color(0xFF8D8E98),
-                        // activeTrackColor: Colors.white,
-                        // overlayColor: Color(0x99EB1555),
-                        // thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                        // overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
-                      ),
-                      child: Slider(
-                        min: 0,
-                        max: 100,
-                        value: _audioSliderPos,
-                        onChanged: (double value) {
-                          setState(() {
-                            _audioSliderPos = value;
-                          });
-                        },
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25)),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50, right: 50, top: 16),
+                      child: Column(
+                        children: [
+                          SliderTheme(
+                            data: SliderThemeData(
+                              // thumbColor: Color(0xFFEB1555),
+                              inactiveTrackColor: Color(0xFF8D8E98),
+                              // activeTrackColor: Colors.white,
+                              // overlayColor: Color(0x99EB1555),
+                              // thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                              // overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                            ),
+                            child: Slider(
+                              min: 0,
+                              max: 100,
+                              value: _audioSliderPos,
+                              onChanged: (double value) {
+                                setState(() {
+                                  _audioSliderPos = value;
+                                });
+                              },
+                            ),
+                          ),
+                          Consumer<CurrentAudio>(
+                              builder: (context, currentAudio, child) => Row(
+                                children: [
+                                  Text(currentAudio.currentAudioPosition.toString().split('.').first),
+                                  Spacer(),
+                                  Text(currentAudio.totalAudioDuration.toString().split('.').first)
+                                ],
+                              )
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50, right: 50, top: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Consumer<CurrentAudio>(
-                          builder: (context, currentAudio, child) => InkWell(
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50, right: 50, top: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Consumer<CurrentAudio>(
+                            builder: (context, currentAudio, child) => InkWell(
+                              onTap: () {
+                                currentAudio.isPlaying ? currentAudio.pauseAudio() : currentAudio.playAudio();
+                              },
+                              child: Container(
+                                child: Center(
+                                    child: Icon( currentAudio.isPlaying ? Icons.pause : Icons.play_arrow, size: 50,
+                                    )),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50, right: 50, top: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
                             onTap: () {
-                              currentAudio.isPlaying ? currentAudio.pauseAudio() : currentAudio.playAudio();
+                              setState(() {
+                                isFavorite = !isFavorite;
+                              });
                             },
                             child: Container(
                               child: Center(
-                                  child: Icon( currentAudio.isPlaying ? Icons.pause : Icons.play_arrow, size: 50,
-                                  )),
+                                  child: IconTheme(
+                                      data: new IconThemeData(color: Colors.redAccent),
+                                      child: Icon(
+                                        isFavorite ? Icons.favorite : Icons.favorite_border,
+                                        size: 50,
+                                      ))),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50, right: 50, top: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            setState(() {
-                              isFavorite = !isFavorite;
-                            });
-                          },
-                          child: Container(
-                            child: Center(
-                                child: IconTheme(
-                                    data: new IconThemeData(color: Colors.redAccent),
-                                    child: Icon(
-                                      isFavorite ? Icons.favorite : Icons.favorite_border,
-                                      size: 50,
-                                    ))),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                    Spacer(),
+                  ],
+                ),
               ),
             ),
-            Spacer(),
-            Spacer(),
           ],
         ),
       ),
