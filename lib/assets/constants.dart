7230ebe88e2179382f1b2ee980e7c3b15b357652
aedorado <annurag94@gmail.com>
2021-06-18 10:38:25 +0530

    Search Audios

--- lib/assets/constants.dart
+++ lib/assets/constants.dart
@@ -1,12 +1,17 @@
 const String HIVE_BOX_FAVORITE_AUDIOS = 'hive_box_favorite_audios';
+
 const String HIVE_BOX_ALERTS = 'hive_box_alerts';
+
+const String HIVE_BOX_AUDIO_SEARCH = 'hive_box_audio_search';
+
+const String HIVE_BOX_AUDIO_SEARCH_KEY_PREVIOUSLY_SEARCHED_LIST =
+    'hive_box_audio_search_key_previously_searched_list';
+
+const String HIVE_BOX_AUDIO_SEARCH_KEY_SELECTED_ITEM =
+    'hive_box_audio_search_key_selected_item';
 
 const String FAVORITES_ACTION_ADD = 'item_added_to_favorites';
 const String FAVORITES_ACTION_REMOVE = 'item_removed_from_favorites';
 
 const String FAVORITE_SNACKBAR_TEXT_ADDED = 'Added to favorites';
 const String FAVORITE_SNACKBAR_TEXT_REMOVED = 'Removed from favorites';
-
-const ADD_TO_QUEUE = 'add_to_queue';
-const REMOVE_FROM_QUEUE = 'remove_from_queue';
-const PLAY_NEXT = 'play_next';
--- lib/ui/audio/audio_list_screen.dart
+++ lib/ui/audio/audio_list_screen.dart
@@ -1,9 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:hh_bbds_app/adapter/adapter.dart';
-import 'package:hh_bbds_app/background/background_audio_controls.dart';
 import 'package:audio_service/audio_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:hh_bbds_app/adapter/adapter.dart';
 import 'package:hh_bbds_app/assets/constants.dart';
+import 'package:hh_bbds_app/background/background_audio_controls.dart';
 import 'package:hh_bbds_app/models/podo/audio.dart';
 import 'package:hh_bbds_app/models/podo/audio_folder.dart';
 import 'package:hh_bbds_app/network/remote_config.dart';
@@ -12,6 +12,11 @@ import 'package:hh_bbds_app/ui/audio/audio_play_screen.dart';
 import 'package:hh_bbds_app/ui/audio/miniplayer.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+
+const TRACKS = 'tracks';
+const SERIES = 'series';
+const SEMINARS = 'seminars';
+const SHORT_AUDIOS = 'short audios';
 
 _backgroundTaskEntrypoint() {
   AudioServiceBackground.run(() => AudioPlayerBackgroundTasks());
@@ -37,20 +42,12 @@ class _AudioListScreenState extends State<AudioListScreen> {
   ];
   final List audioListScreenFutures = [
     FirebaseFirestore.instance.collection("audios").snapshots(),
-    // FirebaseFirestore.instance.collection("audio_folders").where("type", isEqualTo: "series").snapshots(),
     FirebaseFirestore.instance.collection("series").snapshots(),
     FirebaseFirestore.instance.collection("seminars").snapshots(),
     FirebaseFirestore.instance.collection("audios").snapshots(),
-    // FirebaseFirestore.instance.collection("audios").snapshots(),
-    // fetchAudios('https://mocki.io/v1/00c25346-891a-4a2a-987e-4a9c1a6c637e'),
-    // fetchAudioFolders('https://mocki.io/v1/0d9de1d0-332b-4748-bc5a-f5b79b952883'),
-    // fetchAudioFolders('https://mocki.io/v1/0d9de1d0-332b-4748-bc5a-f5b79b952883'),
-    // fetchAudios('https://mocki.io/v1/707d651f-aa2f-4d0a-90a9-2db94623350b'),
-    // fetchAudios('https://mocki.io/v1/00c25346-891a-4a2a-987e-4a9c1a6c637e'),
   ];
 
-  // CurrentAudio currentAudio;
-  // AudioPlayer audioPlayer;
+  bool _isSearching = false;
 
   @override
   void initState() {
@@ -62,17 +59,80 @@ class _AudioListScreenState extends State<AudioListScreen> {
     await AudioService.connect();
   }
 
-  // @override
-  // void didChangeDependencies() {
-  //   currentAudio = Provider.of<CurrentAudio>(context, listen: false);
-  //   audioPlayer = Provider.of<CurrentAudio>(context, listen: false).audioPlayer;
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: !_isSearching,
+        foregroundColor: Colors.black,
         title: Text('Audio Library'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () async {
+                var selectionType =
+                    await showSearch(context: context, delegate: AudioSearch());
+                if (selectionType == TRACKS || selectionType == SHORT_AUDIOS) {
+                  Box hiveBox = Hive.box(HIVE_BOX_AUDIO_SEARCH);
+                  String id =
+                      hiveBox.get(HIVE_BOX_AUDIO_SEARCH_KEY_SELECTED_ITEM);
+                  FirebaseFirestore.instance
+                      .collection('audios')
+                      .where("id", isEqualTo: id)
+                      .limit(1)
+                      .get()
+                      .then(
+                    (value) async {
+                      if (value.size > 0) {
+                        Audio audio =
+                            Adapter.firebaseAudioSnapshotToAudio(value.docs[0]);
+                        MediaItem mediaItem = Adapter.audioToMediaItem(audio);
+                        await AudioService.connect();
+                        if (!AudioService.running) {
+                          await AudioService.start(
+                              androidNotificationIcon: 'mipmap/ic_launcher',
+                              backgroundTaskEntrypoint:
+                                  _backgroundTaskEntrypoint); //, params: {"url": audio.url});
+                        }
+                        if (AudioService.currentMediaItem?.id != mediaItem.id) {
+                          await AudioService.playMediaItem(mediaItem);
+                        }
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => AudioPlayScreen()));
+                      }
+                    },
+                  );
+                } else if (selectionType == SEMINARS ||
+                    selectionType == SERIES) {
+                  Box hiveBox = Hive.box(HIVE_BOX_AUDIO_SEARCH);
+                  String id =
+                      hiveBox.get(HIVE_BOX_AUDIO_SEARCH_KEY_SELECTED_ITEM);
+                  debugPrint(selectionType?.toLowerCase());
+                  FirebaseFirestore.instance
+                      .collection(selectionType!.toLowerCase())
+                      .where("id", isEqualTo: id)
+                      .limit(1)
+                      .get()
+                      .then(
+                    (value) async {
+                      if (value.size > 0) {
+                        AudioFolder audioFolder =
+                            Adapter.firebaseAudioFolderSnapshotToAudioFolder(
+                                value.docs[0]);
+                        debugPrint(audioFolder.toString());
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    AudioFolderScreen(audioFolder)));
+                      }
+                    },
+                  );
+                }
+              }),
+        ],
       ),
       body: SafeArea(
         child: Column(
@@ -178,15 +238,14 @@ class AudioYearList extends StatelessWidget {
         itemCount: yearsList.length,
         itemBuilder: (BuildContext content, int index) {
           return ListTile(
-            title: Text('${yearsList[index]}', style: TextStyle(fontSize: 24.0)),
+            title:
+                Text('${yearsList[index]}', style: TextStyle(fontSize: 24.0)),
             trailing: Icon(Icons.chevron_right),
             onTap: () {
               Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => AudioYearScreen(yearsList[index])
-                  )
-              );
+                      builder: (context) => AudioYearScreen(yearsList[index])));
             },
           );
         });
@@ -491,4 +550,250 @@ class FavoritesSnackBar extends StatelessWidget {
       duration: Duration(milliseconds: 1000),
     );
   }
+}
+
+class AudioSearch extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        query = "";
+        close(context, "false");
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    Box hiveBox = Hive.box(HIVE_BOX_AUDIO_SEARCH);
+
+    if (query.isEmpty) {
+      var suggestionList = hiveBox.get('search_audios_1');
+      suggestionList =
+          (suggestionList == null) ? [] : suggestionList.reversed.toList();
+      return ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+          onTap: () {
+            _updateSavedSuggestions(hiveBox, suggestionList[index]);
+            // HIVE_BOX_AUDIO_SEARCH_SELECTED_ITEM
+            // TODO - pass the type that we select form hive list
+            query = suggestionList[index];
+            // close(context, "true");
+          },
+          leading: Icon(Icons.music_note),
+          title: Text(suggestionList[index]),
+        ),
+        itemCount: suggestionList.length,
+      );
+    } else if (query.length > 2) {
+      return StatefulBuilder(
+        builder: (context, setState) {
+          return StreamBuilder<QuerySnapshot>(
+            stream: _firestoreSnapshots[_selectedSearchFilter],
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var docs = _filterDocs(snapshot.data!.docs, query);
+                return Column(
+                  children: [
+                    Container(
+                      height: 56,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: SingleChildScrollView(
+                              physics: BouncingScrollPhysics(
+                                  parent: AlwaysScrollableScrollPhysics()),
+                              scrollDirection: Axis.horizontal,
+                              child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  shrinkWrap: true,
+                                  itemCount:
+                                      _searchScreenSuggestionBoxes.length,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 5.0,
+                                          right: 5.0,
+                                          top: 6,
+                                          bottom: 6),
+                                      child: AnimatedContainer(
+                                        duration: Duration(milliseconds: 300),
+                                        curve: Curves.easeIn,
+                                        decoration: new BoxDecoration(
+                                          color: this._selectedSearchFilter ==
+                                                  index
+                                              ? Color(0xFF0077C2)
+                                              : Color(0xFFBDDEFB),
+                                          borderRadius: new BorderRadius.all(
+                                              Radius.elliptical(80, 100)),
+                                        ),
+                                        height: 36,
+                                        child: InkWell(
+                                          onTap: () {
+                                            setState(() {
+                                              this._selectedSearchFilter =
+                                                  index;
+                                            });
+                                          },
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Center(
+                                              child: Text(
+                                                _searchScreenSuggestionBoxes[
+                                                        index]
+                                                    .toUpperCase(),
+                                                style: TextStyle(
+                                                    color:
+                                                        this._selectedSearchFilter ==
+                                                                index
+                                                            ? Colors.white
+                                                            : Colors.black,
+                                                    fontSize: 16),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ListView.builder(
+                        physics: BouncingScrollPhysics(
+                            parent: AlwaysScrollableScrollPhysics()),
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemCount: docs.length,
+                        itemBuilder: (context, index) {
+                          // Audio audio =
+                          //     Adapter.firebaseAudioSnapshotToAudio(docs[index]);
+                          return ListTile(
+                            onTap: () {
+                              _updateSavedSuggestions(
+                                  hiveBox, docs[index]['name']);
+                              hiveBox.put(
+                                  HIVE_BOX_AUDIO_SEARCH_KEY_SELECTED_ITEM,
+                                  docs[index]['id']);
+                              close(
+                                  context,
+                                  this._searchScreenSuggestionBoxes[
+                                      this._selectedSearchFilter]);
+                            },
+                            leading: Icon(Icons.music_note),
+                            title: Text(docs[index]['name'] ?? ''),
+                          );
+                        }),
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              return Center(child: CircularProgressIndicator());
+            },
+          );
+        },
+      );
+    } else {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+  }
+
+  void _updateSavedSuggestions(Box hiveBox, String suggestion) {
+    var recentSearches = hiveBox.get('search_audios_1');
+    recentSearches =
+        (recentSearches == null) ? [] : recentSearches.reversed.toList();
+
+    if (!recentSearches.contains(suggestion)) {
+      recentSearches = recentSearches.reversed.toList();
+      recentSearches.add(suggestion);
+    } else {
+      recentSearches = recentSearches.reversed.toList();
+      recentSearches.removeWhere((item) => item == suggestion);
+      recentSearches.add(suggestion);
+    }
+    if (recentSearches.length > 5) {
+      recentSearches.removeAt(0);
+    }
+    hiveBox.put('search_audios_1', recentSearches);
+  }
+
+  List<QueryDocumentSnapshot<Object?>> _filterDocs(
+      List<QueryDocumentSnapshot<Object?>> docs, String query) {
+    query = query.trim().toLowerCase();
+    List<QueryDocumentSnapshot<Object?>> matchedByName = [];
+    List<QueryDocumentSnapshot<Object?>> matchedByTags = [];
+    var matchedSet = new Set();
+
+    if (this._searchScreenSuggestionBoxes[this._selectedSearchFilter] ==
+            SERIES ||
+        this._searchScreenSuggestionBoxes[this._selectedSearchFilter] ==
+            SEMINARS) {
+      matchedByName = docs.where((element) {
+        return (element['name'] as String).toLowerCase().contains(query);
+      }).toList();
+    } else if (this._searchScreenSuggestionBoxes[this._selectedSearchFilter] ==
+            TRACKS ||
+        this._searchScreenSuggestionBoxes[this._selectedSearchFilter] ==
+            SHORT_AUDIOS) {
+      // all audios and short audios
+      matchedByName = docs.where((element) {
+        bool isAMatch =
+            (element['name'] as String).toLowerCase().contains(query);
+        if (isAMatch) matchedSet.add(element['id']);
+        return isAMatch;
+      }).toList();
+      matchedByTags = docs
+          .where((element) => (!matchedSet.contains(element['id']) &&
+              (element['tags'] as String).toLowerCase().contains(query)))
+          .toList();
+    }
+
+    return [...matchedByName, ...matchedByTags];
+  }
+
+  var _searchScreenSuggestionBoxes = [
+    TRACKS,
+    SERIES,
+    SEMINARS,
+    SHORT_AUDIOS,
+  ];
+  int _selectedSearchFilter = 0;
+  var _firestoreSnapshots = [
+    FirebaseFirestore.instance
+        .collection("audios")
+        .where('isShortAudio', isEqualTo: false)
+        .snapshots(),
+    FirebaseFirestore.instance.collection("series").snapshots(),
+    FirebaseFirestore.instance.collection("seminars").snapshots(),
+    FirebaseFirestore.instance
+        .collection("audios")
+        .where('isShortAudio', isEqualTo: true)
+        .snapshots(),
+  ];
 }
